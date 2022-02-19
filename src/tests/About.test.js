5ecbaceea1ff9fada56cf2278e2dc412d857de24
Alan Brazões <alanbrazoes@gmail.com>
2022-02-19 12:55:07 -0300

    req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Tests About page.', () => {
+  test('Should have "About" information.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const paragraph1 = getByText(/This application simulates a Pokédex/i);
+    const paragraph2 = getByText(
+      'One can filter Pokémons by type, and see more details for each one of them',
+    );
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  test('Should have title "About Pokédex".', () => {
+    const { queryByRole } = renderWithRouter(<About />);
+    const title = queryByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Should have two paragraphs.', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragraphs = getAllByText(/Pokémons/i);
+
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Should have image pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img', { name: /pokédex/i });
+
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
